a40eb57381275829cefc5cac445c0eae684d76b9
Sadegh Askari <sm.sadegh.askari@gmail.com>
2024-06-19 19:08:29 +0330

    fix for ComponentAssigner InvalidCastException

--- Editor/Assigners/ComponentAssigner.cs
+++ Editor/Assigners/ComponentAssigner.cs
@@ -24,7 +24,8 @@ namespace AutoAssigner.Assigners
             if (property.objectReferenceValue != null)
                 return true;
 
-            if (!property.HasPrefabInTheName())
+            var isComponent = property.serializedObject.targetObject is Component;
+            if (!property.HasPrefabInTheName() && isComponent)
             {
                 var root = (Component)property.serializedObject.targetObject;
                 Component[] children = root.GetComponentsInChildren(fieldType, true);
